f93821640df3791668b3c10e325b05fa36e5e21b
Ujjwal Sharma <ujjwal3sharma@gmail.com>
2021-07-18 12:10:46 +0530

    All Layout added ->StackLayout ->GridLayout ->GridLayout ->BoxLayout

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from kivy.app import App
+from kivy.metrics import dp
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
+
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # self.orientation = "lr-bt"
+        for i in range(100):
+            # size = dp(100) + i*10
+            size = dp(100)
+            b = Button(text=f"{i+1}", size_hint=(None, None), size=(size, size))
+            self.add_widget(b)
+
+
+# class GridLayoutExample(GridLayout):
+#     pass
+
+
+class GridLayoutExample(AnchorLayout):
+    pass
 
 
 class BoxLayoutExample(BoxLayout):
